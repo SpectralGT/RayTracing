7920f388c4fe9fb12e55a37225fd07c28eb65ee1
Atharv Singh <78777556+SpectralGT@users.noreply.github.com>
2025-02-07 20:43:03 +0530

    fixed errors

--- sphere.h
+++ sphere.h
@@ -2,48 +2,42 @@
 #define SPHERE_H
 
 #include "hittable.h"
-#include "vec3.h"
 
-class sphere : public hittable
-{
+class sphere : public hittable {
 public:
-    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-    bool hit(const ray &r, interval ray_t, hit_record &rec) const override
-    {
-        {
-            vec3 oc = center - r.origin();
-            auto a = r.direction().length_squared();
-            auto h = dot(r.direction(), oc);
-            auto c = oc.length_squared() - radius * radius;
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
 
-            auto discriminant = h * h - a * c;
-            if (discriminant < 0)
-                return false;
+        auto discriminant = h * h - a * c;
+        if (discriminant < 0)
+            return false;
 
-            auto sqrtd = std::sqrt(discriminant);
+        auto sqrtd = std::sqrt(discriminant);
 
-            // Find the nearest root that lies in the acceptable range.
-            auto root = (h - sqrtd) / a;
+        // Find the nearest root that lies in the acceptable range.
+        auto root = (h - sqrtd) / a;
+        if (!ray_t.surrounds(root)) {
+            root = (h + sqrtd) / a;
             if (!ray_t.surrounds(root))
-            {
-                {
-                    root = (h + sqrtd) / a;
-                    if (!ray_t.surrounds(root))
-                        return false;
-                }
+                return false;
+        }
 
-                rec.t = root;
-                rec.p = r.at(rec.t);
-                vec3 outward_normal = (rec.p - center) / radius;
-                rec.set_face_normal(r, outward_normal);
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
 
-                return true;
-            }
+        return true;
+    }
 
-        private:
-            point3 center;
-            double radius;
-        };
+private:
+    point3 center;
+    double radius;
+};
 
 #endif
